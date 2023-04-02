1857cdfde3c0a92894993a758c0fd40b04cc1424
Emanuel <Emanuel0boehm@gmail.com>
2023-04-03 00:26:33 +0200

    add temp ldtk loading This is mostly meant for having a start for tomorrow.

--- src/loading.rs
+++ src/loading.rs
@@ -1,6 +1,9 @@
 use crate::GameState;
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
+use bevy_ecs_ldtk::{
+    prelude::LdtkEntityAppExt, LdtkEntity, LdtkPlugin, LdtkWorldBundle, LevelSelection,
+};
 use bevy_kira_audio::AudioSource;
 
 pub struct LoadingPlugin;
@@ -13,12 +16,28 @@ impl Plugin for LoadingPlugin {
         app.add_loading_state(
             LoadingState::new(GameState::Loading).continue_to_state(GameState::Menu),
         )
+        .add_plugin(LdtkPlugin)
+        .insert_resource(LevelSelection::Index(0))
+        .add_system(load_ldtk.in_schedule(OnEnter(GameState::Playing)))
+        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
         .add_collection_to_loading_state::<_, FontAssets>(GameState::Loading)
         .add_collection_to_loading_state::<_, AudioAssets>(GameState::Loading)
         .add_collection_to_loading_state::<_, TextureAssets>(GameState::Loading);
     }
 }
+pub fn load_ldtk(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("level/placeholder.ldtk"),
+        ..Default::default()
+    });
+}
 
+#[derive(Bundle, LdtkEntity)]
+pub struct MyBundle {
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle,
+}
 // the following asset collections will be loaded during the State `GameState::Loading`
 // when done loading, they will be inserted as resources (see <https://github.com/NiklasEi/bevy_asset_loader>)
 
